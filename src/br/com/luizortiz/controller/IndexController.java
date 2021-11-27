6f96fbaa36f80c26cfcd22c6d64236d616cf22b2
Luiz Ortiz <luizortiz2010@hotmail.com>
2021-11-26 22:32:47 -0300

    26/11/2021

--- src/br/com/luizortiz/controller/IndexController.java
+++ src/br/com/luizortiz/controller/IndexController.java
@@ -4,6 +4,7 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.net.URL;
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 import java.util.ResourceBundle;
 
@@ -12,19 +13,22 @@ import javax.swing.JOptionPane;
 import br.com.luizortiz.io.TarefaIO;
 import br.com.luizortiz.model.StatusTarefa;
 import br.com.luizortiz.model.Tarefa;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.DatePicker;
+import javafx.scene.control.TableCell;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
 
-public class IndexController implements Initializable {
+public class IndexController implements Initializable, ChangeListener<Tarefa> {
 
 	@FXML
 	private TextField tfTarefa;
@@ -113,14 +117,14 @@ public class IndexController implements Initializable {
 			tarefa.setDescricao(tfTarefa.getText());
 			tarefa.setComentario(taComentarios.getText());
 
-			System.out.println(tarefa.formattoSave());
-
 			// TODO Inserir no Banco de Dados
 
 			try {
 				TarefaIO.insert(tarefa);
 				// Limpar os campos
 				limpar();
+				// Carregar as Tarefas
+				carregarTarefas();
 			} catch (FileNotFoundException e) {
 				JOptionPane.showMessageDialog(null, "Arquivo não encontrado: " + e.getMessage(), "Erro",
 						JOptionPane.ERROR_MESSAGE);
@@ -143,6 +147,10 @@ public class IndexController implements Initializable {
 		taComentarios.setText(null);
 		tfStatus.clear();
 		dpData.requestFocus();
+		btAdiar.setDisable(true);
+		btConcluir.setDisable(true);
+		btApagar.setDisable(true);
+		tvTarefa.getSelectionModel().clearSelection();
 
 	}
 
@@ -151,6 +159,23 @@ public class IndexController implements Initializable {
 		// Definir os parâmetros para as colunas do Tableview
 		tcData.setCellValueFactory(new PropertyValueFactory<>("dataLimite"));
 		tcTarefa.setCellValueFactory(new PropertyValueFactory<>("descricao"));
+		tcData.setCellFactory(call -> {
+			return new TableCell<Tarefa, LocalDate>() {
+				@Override
+				protected void updateItem(LocalDate item, boolean empty) {
+					DateTimeFormatter fmt = DateTimeFormatter.ofPattern("dd/MMM/yyyy");
+					if (!empty) {
+						setText(item.format(fmt));
+					} else {
+						setText(" ");
+					}
+				}
+			};
+
+		});
+		// Evento de seleção de item na tabela
+		tvTarefa.getSelectionModel().selectedItemProperty().addListener(this);
+
 		carregarTarefas();
 
 	}
@@ -165,5 +190,26 @@ public class IndexController implements Initializable {
 					JOptionPane.ERROR_MESSAGE);
 			e.printStackTrace();
 		}
+	}
+
+	@Override
+	public void changed(ObservableValue<? extends Tarefa> observable, Tarefa oldValue, Tarefa newValue) {
+
+		// Passo a referência para a variável global
+		tarefa = newValue;
+		if (tarefa != null) {
+			tfTarefa.setText(tarefa.getDescricao());
+			tfAutor.setText(tarefa.getAutor());
+			taComentarios.setText(tarefa.getComentario());
+			tfStatus.setText(tarefa.getStatus() + "");
+			dpData.setValue(tarefa.getDataLimite());
+			btAdiar.setDisable(false);
+			btConcluir.setDisable(false);
+			btApagar.setDisable(false);
+			dpData.setDisable(true);
+			dpData.setOpacity(1);
+
+		}
+
 	}
 }
--- src/br/com/luizortiz/model/StatusTarefa.java
+++ src/br/com/luizortiz/model/StatusTarefa.java
@@ -2,5 +2,20 @@ package br.com.luizortiz.model;
 
 public enum StatusTarefa {
 
-	ABERTA, ADIADA, CONCLUIDA;
+	ABERTA("Aberta"),
+	ADIADA("Adiada"),
+	CONCLUIDA("Concluída");
+
+	private StatusTarefa(String descricao) {
+	this.descricao = descricao;
+		
+	}
+	
+	String descricao;
+
+	@Override
+	public String toString() {
+		return descricao;
+	}
+
 }
